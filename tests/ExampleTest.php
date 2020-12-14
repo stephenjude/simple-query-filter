1248f500ad112386b7ccdf88f309331ec8b547db
Stephen Jude <StephenJudesuccess@gmail.com>
2020-12-14 18:04:18 +0100

    wip: fix sample test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Stephenjude\ApiTestHelper\Tests;
+
+use Orchestra\Testbench\TestCase;
+use Stephenjude\SimpleQueryFilter\SimpleQueryFilterServiceProvider;
+
+class ExampleTest extends TestCase
+{
+
+    protected function getPackageProviders($app)
+    {
+        return [SimpleQueryFilterServiceProvider::class];
+    }
+    
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
